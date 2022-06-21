955a4628f51443170277918539a0495818986106
Андрей Жиянов <aazhiyanov@gmail.com>
2022-06-21 14:08:55 +0300

    Исправлена формула

--- Program.cs
+++ Program.cs
@@ -10,58 +10,69 @@ namespace TransformerLoadLosses
     class Trans
     {
         //Присваиваем значение полям в члену классса Trans
-        public int Unom;
-        public int S;
-        public int Pxx;
-        public double R;
-       
-           //Параметризированный конструктор для классса Trans.
-        public Trans (int Unom, int S, int Pxx, double R)
+        public double Unom;
+        public double Snom;
+        public double Pxx;
+        public double Pкз;
+
+
+        //Параметризированный конструктор для классса Trans.
+        public Trans(int U, int S, int Pxx, int Pкз)
         {
-            this.Unom = Unom;
-            this.S = S;
+            Unom = U;
+            Snom = S;
             this.Pxx = Pxx;
-            this.R = R;
+            this.Pкз = Pкз;
+
         }
-            
+
+        //Метод для определения нагрузки при которой следует подключать параллельный трансформатор
+
+
+        //Метод для построения графиков нагрузки с двумя одинаковыми трансформаторами при которой следует подключать параллельный трансформатор
+
 
 
 
     }
-    class LoadLosses
+    class Program
     {
         static void Main(string[] args)
         {
+            int step = 800;
             //Присваиваем значение полям в объеках PRIME & SECOND
-            Trans PRIME = new Trans(55, 16000, 14805, 0.215f);
-            Trans SECOND = new Trans(55, 16000, 9750, 0.328f);
-
-         
+            Trans PRIME = new Trans(55, 16000, 14805, 18175);
+            Trans SECOND = new Trans(55, 16000, 14805, 18175);
 
 
-            double P1, P2, S1, S2, I, I2;
 
-      //формула для пересечения графиков SAME TRANS
-            S1 = PRIME.Unom * Math.Sqrt(2 * PRIME.Pxx / PRIME.R);  
-            I = S1 / PRIME.Unom;
 
-       //формула для пересечения графиков DIFFERENT TRANS, в приоритете AOMNG
-            S2 = Math.Sqrt((PRIME.S* PRIME.S) * (SECOND.R/(PRIME.R+SECOND.R)) + ((PRIME.Unom * PRIME.Unom) *  SECOND.Pxx / PRIME.R));
-            I2 = S2 / PRIME.Unom;
 
-            Console.Write("Необходимо отключать параллельный трансформатор при нагрузке меньше: " + Math.Round(S2) + " кВт");
-            Console.WriteLine("  и токе меньше: " + Math.Round(I2) + " А");
+
+            //формула для пересечения графиков SAME TRANS
+            double S1 = PRIME.Snom * Math.Sqrt(2 * PRIME.Pxx / PRIME.Pкз);
+            double I1 = S1 / PRIME.Unom;
+
+            ////метод для пересечения графиков DIFFERENT TRANS, в приоритете PRIME
+            //     S2 = Math.Sqrt((PRIME.S* PRIME.S) * (SECOND.R/(PRIME.R+SECOND.R)) + ((PRIME.Unom * PRIME.Unom) *  SECOND.Pxx / PRIME.R));
+            //     I2 = S2 / PRIME.Unom;
+
+            Console.Write("Необходимо отключать параллельный трансформатор при нагрузке меньше: " + Math.Round(S1) + " кВт");
+            Console.WriteLine("  и токе меньше: " + Math.Round(I1) + " А");
 
 
             Console.WriteLine("Нагрузка\t Один\t Параллель");
 
-            for (int i = 0; i <= PRIME.S * 2; i += 800)
+            for (int i = 0; i <= PRIME.Snom * 2; i += step)
             {
                 //формула для графика нагрузочных потерь одного трансформатора
-                P1 = ((i * i) / (PRIME.Unom * PRIME.Unom)) * PRIME.R + PRIME.Pxx;
+                double P1 = Math.Pow((i / PRIME.Snom), 2) * PRIME.Pкз + PRIME.Pxx;
 
                 //формула для графика нагрузочных потерь 2х трансформаторов            
-                P2 = ((i * i) / (SECOND.Unom * SECOND.Unom)) * SECOND.R / 2 + 2 * SECOND.Pxx;
+                double R1 = PRIME.Pкз * Math.Pow(PRIME.Unom / PRIME.Snom, 2);
+                double R2 = SECOND.Pкз * Math.Pow(SECOND.Unom / SECOND.Snom, 2);
+
+                double P2 = Math.Pow((i / PRIME.Snom), 2) * (PRIME.Pкз * SECOND.Pкз / (PRIME.Pкз + SECOND.Pкз)) + (PRIME.Pxx + SECOND.Pxx);
                 Console.WriteLine("{0}\t      {1}\t     {2}\t ", i, Math.Round(P1), Math.Round(P2));
 
             }
