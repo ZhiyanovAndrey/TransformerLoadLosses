c0fd028e6adae6489c4a11385900f2412eaae8ed
Андрей Жиянов <aazhiyanov@gmail.com>
2023-06-05 16:55:48 +0300

    определением наименований объектов

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace TransformerLoadLosses
         static void Main(string[] args)
         {
 
-            
+
             List<ElectricalObjects> electricalObjects = new List<ElectricalObjects>();
             electricalObjects.Add(new OverHeadPowerLine(0.249, 0.427, 4.3));//  ВЛ-110 АС120\19
             electricalObjects.Add(new TransformatorSplitWinding(115, 25000, 9.81, 135000, 26000)); // ОРДТНЖ-25000\110
@@ -24,22 +24,35 @@ namespace TransformerLoadLosses
             {
                 if (item is OverHeadPowerLine)
                 {
-                    Console.WriteLine($"Сопротивления ВЛ {nameof(item)} = ");
+                    Console.WriteLine($"Сопротивления ВЛ  = ");
+                }
+
+                if (item is TransformatorSplitWinding)
+                {
+                    Console.WriteLine($"Сопротивления трансформатора с расщепленной обмоткой  = ");
                 }
 
                 if (item is Transformator)
                 {
-                    Console.WriteLine($"Сопротивления трансформатора {item} = ");
+                    Console.WriteLine($"Сопротивления трансформатора  = ");
                 }
+
                 Console.WriteLine($"Активное = {item.Resistance()}");
                 Console.WriteLine($"Реактивное = {item.Reactance()}");
                 Console.WriteLine($"Полное = {item.Impedance()}");
-                
+                Console.WriteLine();
+
             }
 
+         var obj = new Transformator(27.5, 400, 6.5, 5500, 950);
+            string x = "Сопротивления трансформатора";
 
+            Console.WriteLine($"{obj.GetType().Name} {x.GetHashCode()} = {obj.Reactance()}");
+            Console.WriteLine(obj.GetType().Name);
+            Console.WriteLine(typeof(Transformator).Name);
+            
 
-            //Console.WriteLine($"Resistance {overHeadPowerLine.Resistance()}");
+            //Console.WriteLine($"Resistance {new Transformator(27.5, 400, 6.5, 5500, 950)}");
             //Console.WriteLine($"Reactance {overHeadPowerLine.Reactance()}");
             //Console.WriteLine($"Impedance {overHeadPowerLine.Impedance()}");
 
